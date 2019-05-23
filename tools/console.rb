bfc6881c80c77c1de0b03fe042fb215386e0b883
github username <github email address>
2019-05-23 15:43:22 -0700

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,6 +4,23 @@ require_relative '../config/environment.rb'
 
 # test code goes here
 
-binding.pry
+#lifter objects
+jerry = Lifter.new("Jerry", 10)
+elaine = Lifter.new("Elaine", 20)
+george = Lifter.new("George", 30)
+kramer = Lifter.new("Kramer", 40)
+newman = Lifter.new("Newman", 50)
+puddy = Lifter.new("Puddy", 60)
+
+#gym objects
+barrecor = Gym.new("Barrecor")
+golds = Gym.new("Golds")
+
+#membership
+m1 = Membership.new(20, jerry, golds)
+m2 = Membership.new(30, elaine,barrecor)
+m3 = Membership.new(20, jerry, barrecor)
+
+Pry.start
 
 puts "Gains!"
